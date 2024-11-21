f925b58c440ba3fa9802bb3a0e205473174598b2
unknown <joan_mart_her@hotmail.com>
2024-11-21 17:01:22 +0100

    Añadimos en la tabla Users Authtentication

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require ("express")
 const UserController = require("../controllers/UserController")
 const router = express.Router()
+const {authentication} = require('../middleware/authentication')
 
 router.post("/create",UserController.create)
 router.get("/getAll",UserController.getAll)
-router.put("/update/:id",UserController.update)
+router.put("/update/:id",authentication, UserController.update)
 router.post("/login",UserController.login)
 
 
