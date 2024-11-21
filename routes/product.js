d86944fd78728abf5538afd7e3277978172f63c0
unknown <joan_mart_her@hotmail.com>
2024-11-21 17:22:36 +0100

    Añadimos en la tabla Products authentication para el create, update y delete

--- routes/product.js
+++ routes/product.js
@@ -1,15 +1,16 @@
 const express = require ("express")
 const ProductController = require("../controllers/ProductController")
 const router = express.Router()
+const {authentication} = require('../middleware/authentication')
 
-router.post("/create",ProductController.create)
-router.get("/getAll",ProductController.getAll)
-router.put("/update/:id",ProductController.update)
-router.delete("/delete/:id",ProductController.delete)
-router.get("/getById/:id",ProductController.getById)
-router.get("/getByName/:name",ProductController.getByName)
-router.get("/getByPrice/:price",ProductController.getByPrice)
-router.get("/orderByPriceD",ProductController.orderByPriceD)
+router.post("/create", authentication, ProductController.create)
+router.get("/getAll", ProductController.getAll)
+router.put("/update/:id", authentication, ProductController.update)
+router.delete("/delete/:id", authentication, ProductController.delete)
+router.get("/getById/:id", ProductController.getById)
+router.get("/getByName/:name", ProductController.getByName)
+router.get("/getByPrice/:price", ProductController.getByPrice)
+router.get("/orderByPriceD", ProductController.orderByPriceD)
 
 
 
